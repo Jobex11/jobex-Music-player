e4a9dbd7ff09fbd41305ecc18d7c0d62cfa403aa
Lawal Onimisi Job <107330487+Jobex11@users.noreply.github.com>
2023-03-09 14:18:21 +0100

    Add files via upload

---
+++ musicPlayer/music-list.js
@@ -0,0 +1,42 @@
+// evaluation on test
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Scho",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "ikson Anywhere - Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "Losk Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6",
+  },
+
+  //To copy more songs: copy following codee and paste it here
+];
